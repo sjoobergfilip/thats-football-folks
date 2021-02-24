faffa3c810756b839581da3ab7a8704c24b060a5
Filip Sjöberg <filip.sjoberg@hotmail.com>
2021-02-24 23:04:41 +0100

    adding a first page with a background img

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
-    return <div>This is home</div>;
+    return (
+        <>
+            <div className="hero-container">
+                <div className="slogan-container">
+                    <h1>We are That's football folks!</h1>
+                    <p>
+                        I'm baby bicycle rights la croix swag, try-hard drinking
+                        vinegar authentic four loko jean shorts banh mi tattooed
+                        retro selvage iceland. Mixtape snackwave taxidermy
+                        waistcoat, farm-to-table{" "}
+                    </p>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Home;
